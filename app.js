7291d823c775cce560b860ec69a24f59a96acdca
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-20 15:08:35 -0800

    Display gif using handlebars

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-});
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+})
 // Start Server
 
 app.listen(3000, () => {
