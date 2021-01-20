2736030c2a40120a10a1d85716fbb8d1cd89dd4e
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-20 15:06:20 -0800

    add handlebars middleware

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
