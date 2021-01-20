98274cfd751aa87d26f5659be0eb1e1c5ac59549
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-20 14:43:41 -0800

    add app.js file

---
+++ app.js
@@ -0,0 +1,15 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+
+// Routes
+
+// Start Server
+
+app.listen(3000, () => {
+    console.log('Gif Search listening on port localhost:3000!');
+});
